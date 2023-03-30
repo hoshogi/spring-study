1837d62c18c2233a3a1c3ecb9d3586fd6bbb7e87
hoshogi <hoshogi@gmail.com>
2023-03-30 18:50:26 +0900

    JPA프로그래밍 - 기본편: 연관관계 매핑 시작

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,9 +1,12 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,7 +19,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-
 
             tx.commit();
         } catch(Exception e) {
